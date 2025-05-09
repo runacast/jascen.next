10752ef2f6da43aed3bd9da20295af8cd4fb150d
Invers3 <166087818+runacast@users.noreply.github.com>
2025-05-09 11:36:52 -0500

    Primer commit

---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+export default function Page(){
+    return <div>
+        <p>Página de entrada</p>
+    </div>
+}
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+export default function Header(){
+    return <div>
+        <nav>Navegación</nav>
+    </div>
+}
