7fdfd65898ffadf8c30933544d896b5823a07acd
Invers3 <166087818+runacast@users.noreply.github.com>
2025-06-13 10:01:03 -0500

    Update users.js
    Testing

--- src/api/users.js
+++ src/api/users.js
@@ -4,21 +4,21 @@ const mongoClient = new MongoClient(process.env.MONGODB_URI)
 
 const clientPromise = mongoClient.connect()
 
-const handler = async (event) => {
-    try {
-        const database = (await clientPromise).db('jascen_man')
-        const collection = database.collection('users')
-        // Function logic here ...
-        const results = await collection.find({}).limit(10).toArray()
-        return {
-            statusCode: 200,
-            body: JSON.stringify(results),
+const envfr = {
+    handler: async (event) => {
+        try {
+            const database = (await clientPromise).db('jascen_man')
+            const collection = database.collection('users')
+            // Function logic here ...
+            const results = await collection.find({}).limit(10).toArray()
+            return {
+                statusCode: 200,
+                body: JSON.stringify(results),
+            }
+        } catch (error) {
+            return { statusCode: 500, body: error.toString() }
         }
-    } catch (error) {
-        return { statusCode: 500, body: error.toString() }
     }
 }
 
-export default {
-    handler
-}
+export default envfr
