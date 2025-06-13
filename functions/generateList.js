3e0ada86674bd738e87b38137d8ab8e3fb2cfd4a
Invers3 <166087818+runacast@users.noreply.github.com>
2025-06-13 00:01:36 -0500

    Fix node
    Testing

--- functions/generateList.js
+++ functions/generateList.js
@@ -1,7 +1,3 @@
-import fs from 'fs'
-import path from 'path'
-
 exports.handler = async function(){
-    const outpath = path.join('src', 'index', 'list.json')
-    fs.writeFileSync(outpath, '{}')
+    
 }
--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,27 +1,26 @@
-import { ObjectId, MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb'
 
 const uri = process.env.MONGODB_URI
 const options = {}
 
-
-let client;
-let clientPromise;
+let client
+let clientPromise
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Error uri not defined.');
+  throw new Error('Error uri not defined.')
 }
 
 if (process.env.NODE_ENV === 'development') {
   // Guardar conexión en caché
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = global._mongoClientPromise
 } else {
   // En producción sin caché global
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
 }
 
 export default clientPromise
