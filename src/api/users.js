9335bb328fe3934ce706ecb36a3299156580e2c3
Invers3 <166087818+runacast@users.noreply.github.com>
2025-06-13 10:08:19 -0500

    Update users.js
    Testing script

--- src/api/users.js
+++ src/api/users.js
@@ -1,24 +1,11 @@
-import MongoClient from 'mongodb'
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions"
 
-const mongoClient = new MongoClient(process.env.MONGODB_URI)
-
-const clientPromise = mongoClient.connect()
-
-const envfr = {
-    handler: async (event) => {
-        try {
-            const database = (await clientPromise).db('jascen_man')
-            const collection = database.collection('users')
-            // Function logic here ...
-            const results = await collection.find({}).limit(10).toArray()
-            return {
-                statusCode: 200,
-                body: JSON.stringify(results),
-            }
-        } catch (error) {
-            return { statusCode: 500, body: error.toString() }
-        }
+const handler = async (event, context) => {
+    const value = 'Netlify value'
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${value}.`}),
     }
 }
 
-export default envfr
+export { handler }
