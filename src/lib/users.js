295edd8d4be73ceae4d6623a8b37e51194ccf8dc
Invers3 <166087818+runacast@users.noreply.github.com>
2025-06-16 13:54:31 -0500

    Update users.js

--- src/lib/users.js
+++ src/lib/users.js
@@ -1,27 +1,8 @@
 'use server'
 
-const baseUrl = process.env.URL || 'http://localhost:3000'
+const baseUrl = process.env.URL || process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:8888'
 
 export async function get(form = undefined){
-
-    
-    
-    /*if (form) {
-        const user = await collection.findOne(form)
-
-        if (user) return {
-            ...user,
-            _id: user._id.toString()
-        }
-        return user
-    }
-
-    const users = await collection.find({}).toArray()
-
-    if (users) return users.map(user => ({
-        ...user,
-        _id: user._id.toString()
-        }))*/
     let payload = {}
 
     if(form){
@@ -29,23 +10,26 @@ export async function get(form = undefined){
     }
 
     const url = new URL('/.netlify/functions/users', baseUrl)
+
     Object.entries(payload).forEach(([key, value]) =>
         url.searchParams.append(key, value)
     )
-    
+
     const response = await fetch(url.toString(), {
         method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
+        headers: {
+            'Accept': 'application/json' // mejor para GET
+        }
     })
-    
+
     if (!response.ok) throw new Error('Error get users')
     return await response.json()
-    
 }
 
 export async function post(form) {
+    let payload = {}
 
-    if(form){
+    if (form){
         payload = Object.fromEntries(form.entries())
     }
 
@@ -55,53 +39,6 @@ export async function post(form) {
         body: JSON.stringify(payload)
     })
 
-    if (!response.ok) throw new Error('Error get users')
+    if (!response.ok) throw new Error('Error post user')
     return await response.json()
-
-    /* Users */
-    /*let response
-    const db = await mongo()
-
-    if (db.statusCode == 500) {
-        return {error:"Error fail connection mongoDB."}
-    }
-    
-    const collection = db.collection('users'),
-    total = await collection.countDocuments()
-    
-    let user = {
-        cod: parseInt(form.get('codigo'), 10),
-        surnames: form.get('apellidos'),
-        names: form.get('nombres'),
-        alias: form.get('apodo'),
-        cid: parseInt(form.get('cedula_id'), 10),
-        phone: form.get('telefono'),
-        email: form.get('correo'),
-        status: true
-    }
-    
-    if(form.get('id')){ // Update user
-        response = await collection.updateOne({ _id: new ObjectId(form.get('id')) },{ $set: user })
-    }else{ // Add new user
-        user.cod = total + 1
-        response = await collection.insertOne(user)
-    }*/
-}
-
-export async function del(form){
-
-    let response
-    
-    const db = await mongo()
-
-    if(db.statusCode == 500){
-        return {error:"Error fail connection mongoDB."}
-    }
-
-    const collection = db.collection('users')
-    response = await collection.deleteOne({ _id: new ObjectId(form.get('id')) })
-
-    return {
-        deletes : response.deletedCount
-    }
 }
