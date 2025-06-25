dfa4168672e3a70c32795b98c532a4b847ecaa4e
Invers3 <166087818+runacast@users.noreply.github.com>
2025-06-25 16:04:28 -0500

    Create test.js

---
+++ netlify/functions/test.js
@@ -0,0 +1,29 @@
+import { connectDB } from '../../src/lib/db'
+import User from '../../src/models/User'
+
+export const handler = async (event) => {
+
+  try {
+    
+    await connectDB()
+
+    const users = await User.find({})
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(users)
+    }
+
+  }catch(err){
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error en el servidor', detail: err.message })
+    }
+  }
+    
+}
+
+export {
+    handler
+}
