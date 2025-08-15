332bb0a03b289746701b5b147c78b8bbd2459430
runacast <166087818+runacast@users.noreply.github.com>
2025-08-14 22:01:45 -0500

    Update page.js
    Testing

--- src/app/(admin)/admin/users/page.js
+++ src/app/(admin)/admin/users/page.js
@@ -1,9 +1,10 @@
 import Users from '@/components/admin/Users'
-import User from '@/models/User.js'
+import User from '../../../../models/User.js'
 
 export default async function adminUsers() {
     
-    const list = await User.find()
+    const user = await User.findOne({ cod: 1 })
+    const list = {meta_title:"Woob"}
     
     return <div className='ses-panel'>
         <Users headerData={list} />
