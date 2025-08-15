b7590c82654150da9208313585b7a6291180e308
runacast <166087818+runacast@users.noreply.github.com>
2025-08-15 02:21:46 -0500

    Update users.js
    Testing

--- netlify/functions/users.js
+++ netlify/functions/users.js
@@ -1,22 +1,15 @@
 import { connectDB } from '../../src/lib/db.js'
 import User from '../../src/models/User.js'
 
-export default async (event, context) => {
+export default async (req, context) => {
   
   try{
+
+    const {test} = await req.json()
     
     await connectDB()
-
-    const { key } = context.params
     
-    return {
-      headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        },
-      body: JSON.stringify({ message: "Hello World", key }),
-      statusCode: 200
-    }
+    return new Response(JSON.stringify({"message":`any: ${test}`}))
 
   } catch(e) {
 
