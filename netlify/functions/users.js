695c83fa5c748c45dc8b1c43ad964003e695ac18
runacast <166087818+runacast@users.noreply.github.com>
2025-08-15 02:16:04 -0500

    Update users.js
    Testing

--- netlify/functions/users.js
+++ netlify/functions/users.js
@@ -1,9 +1,24 @@
 import { connectDB } from '../../src/lib/db.js'
 import User from '../../src/models/User.js'
 
-export default async (req, context) => {
-  const {key} = context.params
-  return new Response({
-    "title":key
-  })
+export default async (event, context) => {
+  
+  try{
+    
+    await connectDB()
+
+    const { key } = context.params
+    
+    return {
+      headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+      body: JSON.stringify({ message: "Hello World", key }),
+      statusCode: 200
+    }
+
+  } catch(e) {
+
+  }
 }
